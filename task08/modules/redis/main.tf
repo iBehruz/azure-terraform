338949f72f11f552acffdeab2552f9874bc21549
iBehruz <61056456+iBehruz@users.noreply.github.com>
2025-09-28 20:49:46 +0500

    fix

--- task08/modules/redis/main.tf
+++ task08/modules/redis/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_redis_cache" "main" {
-  name                = var.redis_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  capacity            = 2
-  family              = "C"
-  sku_name            = "Basic"
-  enable_non_ssl_port = false
-  minimum_tls_version = "1.2"
+  name                 = var.redis_name
+  location             = var.location
+  resource_group_name  = var.resource_group_name
+  capacity             = 2
+  family               = "C"
+  sku_name             = "Basic"
+  non_ssl_port_enabled = false
+  minimum_tls_version  = "1.2"
 
   redis_configuration {}
 
--- task08/terraform.tfvars
+++ task08/terraform.tfvars
@@ -1,9 +1,9 @@
 name_prefix  = "cmtr-31zawnrd-mod8"
 location     = "West US"
 acr_sku      = "Basic"
-git_repo_url = "https://github.com/username/repo.git"
+git_repo_url = "https://github.com/Azure-Samples/acr-build-helloworld-node.git"
 
-# git_pat = "your-git-personal-access-token"  # Uncomment and set for production
+git_pat = "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"
 
 tags = {
   Creator = "behroz_ilhomov@epam.com"
