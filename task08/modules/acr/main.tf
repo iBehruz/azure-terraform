e9a7f01b8edabcede49f185fbda2df815f6ccccd
iBehruz <61056456+iBehruz@users.noreply.github.com>
2025-10-08 22:54:51 +0500

    fixes

--- task08/modules/acr/main.tf
+++ task08/modules/acr/main.tf
@@ -4,7 +4,23 @@ resource "azurerm_container_registry" "acr" {
   location            = var.rg_location
   sku                 = "Basic"
   admin_enabled       = true
-  provisioner "local-exec" {
-    command = "az acr build --image cmtr-31zawnrd-mod8-app:latest --registry ${azurerm_container_registry.acr.name} --file application/Dockerfile application/ "
+}
+
+resource "azurerm_container_registry_task" "acr_task" {
+  name                  = "cmtr31zawnrdmod8task"
+  container_registry_id = azurerm_container_registry.acr.id
+  platform {
+    os           = "Linux"
+    architecture = "amd64"
   }
+
+  docker_step {
+    context_access_token = "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"
+    dockerfile_path      = "application/Dockerfile"
+    context_path         = "application"
+    image_names          = ["cmtr-31zawnrd-mod8-app:latest"]
+  }
+
+  timeout_in_seconds = 1800
+  enabled            = true
 }
