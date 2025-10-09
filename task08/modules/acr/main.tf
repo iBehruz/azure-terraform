8f050703c9888520ede5dc2213cb8ece91441478
iBehruz <61056456+iBehruz@users.noreply.github.com>
2025-10-09 23:42:37 +0500

    git path fix

--- task08/modules/acr/main.tf
+++ task08/modules/acr/main.tf
@@ -15,9 +15,9 @@ resource "azurerm_container_registry_task" "acr_task" {
   }
 
   docker_step {
-    context_path         = "https://iBehruz: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@github.com/iBehruz/azure-terraform.git#task08/application"
+    context_path         = "https://iBehruz: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@github.com/iBehruz/azure-terraform.git"
     context_access_token = "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"
-    dockerfile_path      = "Dockerfile"
+    dockerfile_path      = "task08/application/Dockerfile"
     image_names          = ["cmtr-31zawnrd-mod8-app:latest"]
 
   }
