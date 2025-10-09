b52112d8d964fb014956837f9b506f2f51a65ae5
iBehruz <61056456+iBehruz@users.noreply.github.com>
2025-10-09 23:59:35 +0500

    kubectl fix host

--- task08/modules/aks/main.tf
+++ task08/modules/aks/main.tf
@@ -43,3 +43,10 @@ resource "azurerm_key_vault_access_policy" "aks" {
 
   secret_permissions = ["Get", "List"]
 }
+
+provider "kubectl" {
+  host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate)
+}
