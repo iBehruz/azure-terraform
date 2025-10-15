999e328a5ff473fe99e608188157e7919b1c23ce
iBehruz <61056456+iBehruz@users.noreply.github.com>
2025-10-15 14:36:50 +0500

    kube cofig

--- task08/modules/aks/outputs.tf
+++ task08/modules/aks/outputs.tf
@@ -1,12 +1,8 @@
-output "kube_config" {
-  value = {
-    host                   = azurerm_kubernetes_cluster.main.kube_config[0].host
-    client_certificate     = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
-    client_key             = azurerm_kubernetes_cluster.main.kube_config[0].client_key
-    cluster_ca_certificate = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
-  }
-  sensitive = true
-}
+output "host"                   { value = azurerm_kubernetes_cluster.main.kube_config[0].host }
+output "client_certificate"     { value = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate }
+output "client_key"             { value = azurerm_kubernetes_cluster.main.kube_config[0].client_key }
+output "cluster_ca_certificate" { value = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate }
+
 
 output "kubelet_identity_object_id" {
   value = azurerm_kubernetes_cluster.main.key_vault_secrets_provider[0].secret_identity[0].object_id
