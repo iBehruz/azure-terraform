84c135645a6fee4711ab8b3058f0d837a675f961
iBehruz <61056456+iBehruz@users.noreply.github.com>
2025-10-26 14:19:45 +0500

    fix

--- task08/main.tf
+++ task08/main.tf
@@ -80,11 +80,11 @@ data "azurerm_kubernetes_cluster" "main" {
 
 provider "kubectl" {
   alias                  = "alekc"
-  host                   = data.azurerm_kubernetes_cluster.main.kube_config.0.host
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
-  apply_retry_count      = 5
+  load_config_file = false
+  host = data.azurerm_kubernetes_cluster.main.kube_admin_config[0].host
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.main.kube_admin_config[0].cluster_ca_certificate)
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.main.kube_admin_config[0].client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.main.kube_admin_config[0].client_key)
 }
 
 
